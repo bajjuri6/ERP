af1e279abc3e6b310b0e20ba5bf7791107dfc47b
venugopaltattari <venu.tattari@viveninfomedia.com>
2013-07-27 10:06:27 +0530

    Linking enquiry form to database

--- application/modules/business/controllers/enquiry.php
+++ application/modules/business/controllers/enquiry.php
@@ -8,101 +8,127 @@ class Viven_Business_Enquiry extends Controller{
   
   function newAction(){
     if(isset($_POST['enq'])){
-      //process the enquiry on submit
-    }else{
+      foreach($_POST as $option){
+        if(!$option){
+          $error = true;
+          break;
+        }
+      }
       
-      $form = new Form();
-      $form_fields = array();
-      
-      /**
-       * Enquiry Form Elements
-       */
-      $date = array("type" => "input", 
-                  "name" => "date",
-                  "id" => "date",
-                  "size" => "27",
-                  "readonly" => "readonly",
-                  "class" => "none datepicker");
-      $adate = $form -> Viven_AddInput($date);
-      $form_fields['Date:'] = $adate;
-      
-      
-      $cn = array("type" => "text", 
-                  "name" => "cn",
-                  "id" => "cn",
-                  "size" => "27",
-                  "class" => "none");
-      $cname = $form -> Viven_AddInput($cn);      
-      $form_fields['Customer Name:'] = $cname;
-      
-      
-      $pn = array("type" => "text", 
-                  "name" => "pn",
-                  "id" => "pn",
-                  "size" => "27",
-                  "class" => "none");
-      $pnumber = $form -> Viven_AddInput($pn);      
-      $form_fields['Phone Number:'] = $pnumber;
-      
-      
-      $em = array("type" => "text", 
-                  "name" => "em",
-                  "id" => "em",
-                  "size" => "27",
-                  "class" => "none");
-      $email = $form -> Viven_AddInput($em);
-      $form_fields['Email Address:'] = $email;
-      
-      
-      $ques = array("type" => "text", 
-                  "name" => "question",
-                  "id" => "question",
-                  "rows" => "3",          
-                  "cols" => "28",
-                  "class" => "none");
-      $question = $form -> Viven_AddText($ques);
-      $form_fields['Enquiry:'] = $question;
-      
-      
-      $fd = array("type" => "text", 
-                  "name" => "fd",
-                  "id" => "fd",
-                  "size" => "27",
-                  "readonly" => "readonly",
-                  "class" => "none datepicker");
-      $fdate = $form -> Viven_AddInput($fd);      
-      $form_fields['Follow Up Date:'] = $fdate;
-      
-      
-      $inc = array("type" => "text", 
-                  "name" => "incharge",
-                  "id" => "incharge",
-                  "size" => "27",
-                  "class" => "none");
-      $incharge = $form -> Viven_AddInput($inc);
-      $form_fields['Follow Up Incharge:'] = $incharge;
-      
-      
-      $remarks = array("type" => "input", 
-                  "name" => "remarks",
-                  "id" => "remarks",
-                  "rows" => "3",          
-                  "cols" => "28",
-                  "class" => "none");
-      $aremarks = $form ->Viven_AddText($remarks);
-      $form_fields['Comments:'] = $aremarks;
-      
-      $enq = array("type" => "hidden", 
-                   "name" => "enq",
-                   "value" => "1");
-      $enquiry = $form -> Viven_AddInput($enq);
-      $form_fields[''] = $enquiry;
-      
-      $outForm = $form -> Viven_ArrangeForm($form_fields,2,0,false);
-      
-      $this -> view-> enquiryform = $outForm;
-      
+      if(isset($error)){
+        $this -> view -> msg = 'All fields are required!';
+      }
+      else{
+        require MODULES . '/business/models/enquiry.php';
+        $model = new Viven_Model_Enquiry();
+        $res = $model -> addEnquiry($_POST);
+        var_dump($res);
+        if($res){
+          $this -> view -> msg = 'Record added successfully!!';
+        }
+        else{
+          $this -> view -> msg = 'Error in processing. Please try after sometime.';
+        }
+      }
     }
+    
+    $form = new Form();
+    $form_fields = array();
+
+    /**
+     * Enquiry Form Elements
+     */
+    $date = array("type" => "input", 
+                "name" => "date",
+                "id" => "date",
+                "size" => "27",
+                "readonly" => "readonly",
+                "class" => "none datepicker");
+    $adate = $form -> Viven_AddInput($date);
+    $form_fields['Date:'] = $adate;
+
+
+    $cn = array("type" => "text", 
+                "name" => "cn",
+                "id" => "cn",
+                "size" => "27",
+                "class" => "none");
+    $cname = $form -> Viven_AddInput($cn);      
+    $form_fields['Customer Name:'] = $cname;
+
+
+    $pn = array("type" => "text", 
+                "name" => "pn",
+                "id" => "pn",
+                "size" => "27",
+                "class" => "none");
+    $pnumber = $form -> Viven_AddInput($pn);      
+    $form_fields['Phone Number:'] = $pnumber;
+
+
+    $em = array("type" => "text", 
+                "name" => "em",
+                "id" => "em",
+                "size" => "27",
+                "class" => "none");
+    $email = $form -> Viven_AddInput($em);
+    $form_fields['Email Address:'] = $email;
+
+    $incType = array("type" => "text", 
+                "name" => "en_type",
+                "id" => "en_type",
+                "size" => "27",
+                "class" => "none");
+    $type = $form -> Viven_AddInput($incType);
+    $form_fields['Enquiry Type:'] = $type;
+    
+    $ques = array("type" => "text", 
+                "name" => "question",
+                "id" => "question",
+                "rows" => "3",          
+                "cols" => "28",
+                "class" => "none");
+    $question = $form -> Viven_AddText($ques);
+    $form_fields['Enquiry comments:'] = $question;
+
+
+    $fd = array("type" => "text", 
+                "name" => "fd",
+                "id" => "fd",
+                "size" => "27",
+                "readonly" => "readonly",
+                "class" => "none datepicker");
+    $fdate = $form -> Viven_AddInput($fd);      
+    $form_fields['Follow Up Date:'] = $fdate;
+
+    $inc = array("type" => "text", 
+                "name" => "incharge",
+                "id" => "incharge",
+                "size" => "27",
+                "class" => "none");
+    $incharge = $form -> Viven_AddInput($inc);
+    $form_fields['Follow Up Incharge:'] = $incharge;
+
+
+    $remarks = array("type" => "input", 
+                "name" => "remarks",
+                "id" => "remarks",
+                "rows" => "3",          
+                "cols" => "28",
+                "class" => "none");
+    $aremarks = $form ->Viven_AddText($remarks);
+    $form_fields['Followup Comments:'] = $aremarks;
+
+    $enq = array("type" => "hidden", 
+                 "name" => "enq",
+                 "value" => "1");
+    $enquiry = $form -> Viven_AddInput($enq);
+    $form_fields[''] = $enquiry;
+
+    $outForm = $form -> Viven_ArrangeForm($form_fields,2,0,false);
+
+    $this -> view-> enquiryform = $outForm;
+      
     $this -> view -> render('enquiry/index','business');
   }
 }
---
+++ application/modules/business/models/enquiry.php
@@ -0,0 +1,33 @@
+<?php
+
+class Viven_Model_Enquiry extends Model{
+
+  function __construct() {
+    parent::__construct();
+  }
+  
+  function addEnquiry($data){
+    $timeCreated = time();
+    $query = "INSERT INTO `viv_inq_en` (`_inq_emp_un`, `_inq_branch`, `_inq_cus_name`, `_inq_cus_pnum`,
+              `_inq_cus_email`, `_inq_time`, `_inq_ref`, `_inq_type`, `_inq_cmnts`, `_inq_ref_cmnts`, `_inq_status`, `_inq_addedby`, 
+              `_inq_addedon`, `_inq_lastmodby`, `_inq_lastmodon`) 
+              VALUES (".$this -> db -> quote($_SESSION['un']).","
+                      .$this -> db -> quote($_SESSION['branch']).","
+                      .$this -> db -> quote($data['cn']).","
+                      .$this -> db -> quote($data['pn']).","
+                      .$this -> db -> quote($data['em']).","
+                      .$this -> db -> quote($timeCreated).","
+                      .$this -> db -> quote($data['incharge']).","
+                      .$this -> db -> quote($data['en_type']).","
+                      .$this -> db -> quote($data['question']).","
+                      .$this -> db -> quote($data['remarks']).","
+                      .$this -> db -> quote(1).","
+                      .$this -> db -> quote($_SESSION['un']).","
+                      .$this -> db -> quote($timeCreated).","
+                      .$this -> db -> quote($_SESSION['un']).","
+                      .$this -> db -> quote($timeCreated).")";
+    $this -> db -> exec($query);
+    
+    return $this -> db -> errorInfo();
+  }
+}
