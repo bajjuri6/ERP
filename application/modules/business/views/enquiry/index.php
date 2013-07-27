0cd91094c432ec3bb0b1c8e03f3c1443acc8f8d8
venugopaltattari <venu.tattari@viveninfomedia.com>
2013-07-27 10:06:16 +0530

    Showing message to user

--- application/modules/business/views/enquiry/index.php
+++ application/modules/business/views/enquiry/index.php
@@ -2,6 +2,9 @@
   <h1>Customer Enquiry</h1>
   <form method="post" action="/business/enquiry/new">
   <?php
+  if(isset($this -> msg)){
+    echo $this -> msg;
+  }
     echo $this -> enquiryform;
   ?>
   </form> 
