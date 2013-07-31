52b031b9e187a18b055ecf716339392129ca3c5b
bajjuri6 <bajjuri@viveninfomedia.com>
2013-08-01 01:30:03 +0530

    Escaping session username and branch for global availability

--- library/Model.php
+++ library/Model.php
@@ -1,9 +1,11 @@
 <?php
 
 class Model extends PDO{
-
+  protected 
   function __construct() {
     $this -> db = new PDO('mysql:host=localhost;dbname=gym_v2', 'root', 'vivenfarms');
+    $this -> eun = $this -> db -> qote($_SESSION['un']);
+    $this -> ebranch = $this -> db -> qote($_SESSION['branch']);
   }
 
 }
