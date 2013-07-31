d2441654af75bea937af9c123d45c2bf665e1783
bajjuri6 <bajjuri@viveninfomedia.com>
2013-08-01 01:34:23 +0530

    Sorry, some grabage lefft over

--- library/Model.php
+++ library/Model.php
@@ -1,7 +1,6 @@
 <?php
 
 class Model extends PDO{
-  protected 
   function __construct() {
     $this -> db = new PDO('mysql:host=localhost;dbname=gym_v2', 'root', 'vivenfarms');
     $this -> eun = $this -> db -> qote($_SESSION['un']);
