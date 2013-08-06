51915cd6c639ba027dc9d8c39e0d3635daa52151
bajjuri6 <bajjuri@viveninfomedia.com>
2013-08-07 01:30:35 +0530

    Updating the query to take care of missing cloumns: inq_question, flwup_date

--- application/modules/business/models/enquiry.php
+++ application/modules/business/models/enquiry.php
@@ -8,17 +8,17 @@ class Viven_Model_Enquiry extends Model{
   
   function addEnquiry($data){
     $timeCreated = time();
-    $query = "INSERT INTO `viv_inq_en` (`_inq_emp_un`, 
+    $qs = "INSERT INTO `viv_inq_en` (`_inq_emp_un`, 
                                         `_inq_branch`, 
                                         `_inq_cus_name`, 
                                         `_inq_cus_pnum`,
                                         `_inq_cus_email`, 
-                                        `_inq_time`, 
-                                        `_inq_ref`, 
+                                        `_inq_time`,
                                         `_inq_type`, 
-                                        `_inq_cmnts`, 
-                                        `_inq_ref_cmnts`, 
-                                        `_inq_status`, 
+                                        `_inq_inq_cmnts`, 
+                                        `_inq_flwup_inc`, 
+                                        `_inq_flwup_date`, 
+                                        `_inq_cmnts`,
                                         `_inq_addedby`,
                                         `_inq_addedon`, 
                                         `_inq_lastmodby`, 
@@ -29,15 +29,21 @@ class Viven_Model_Enquiry extends Model{
                                               .$this -> db -> quote($data['pn']).","
                                               .$this -> db -> quote($data['em']).","
                                               .$this -> db -> quote($timeCreated).","
-                                              .$this -> db -> quote($data['incharge']).","
-                                              .$this -> db -> quote($data['en_type']).","
+                                              .$this -> db -> quote($data['enqtype']).","
                                               .$this -> db -> quote($data['question']).","
+                                              .$this -> db -> quote($data['sn']).","
+                                              .$this -> db -> quote($data['fd']).","
                                               .$this -> db -> quote($data['remarks']).","
-                                              .$this -> db -> quote(1).","
                                               .$this -> db -> quote($_SESSION['un']).","
                                               .$this -> db -> quote($timeCreated).","
                                               .$this -> db -> quote($_SESSION['un']).","
                                               .$this -> db -> quote($timeCreated).")";
-    return $this -> db -> exec($query);
+  
+    if($this -> db -> exec($qs)){
+      return "SUCCESS";
+    }
+    else {
+      return $qs;
+    }
   }
 }
