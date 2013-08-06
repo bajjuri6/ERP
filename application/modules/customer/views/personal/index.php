72e526a7194deced95fe2fb5a324c1be42a3865e
bajjuri6 <bajjuri@viveninfomedia.com>
2013-08-06 22:46:44 +0530

    Provisioning addition of Customer's personal information

---
+++ application/modules/customer/views/personal/index.php
@@ -0,0 +1,9 @@
+<div id='per-form' class='bodyworks'>
+  <h1>Customer Personal Information</h1>
+      
+  <form method="post" id="vf_cp">
+    <?php
+      echo $this -> personal;
+    ?>
+  </form> 
+</div>
