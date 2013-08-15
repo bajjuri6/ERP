d217e98159c4b9043d8ad5bbeae58f62b75f3b75
bajjuri6 <bajjuri@viveninfomedia.com>
2013-08-15 18:34:00 +0530

    on user login, they will now see reports based on their user roles

--- application/modules/user/views/account/home.php
+++ application/modules/user/views/account/home.php
@@ -1,26 +1,77 @@
 <?php
-  if($_SESSION['level'] == 1){
+  require 'library/headers.php';
+  if($_SESSION['level'] == 'Admin'){
 ?>
-
     <div class="bodyworks">
-      <h2>Part One</h2>
-      <?php
-        echo $this -> data1;
-      ?>
+      <h2>Revenues</h2>
+      <table class="reports-table">
+        <?php
+
+          foreach($ARH as $k){
+            $tmp = explode(',' ,$k);
+            print '<th class="' . $tmp[0] . '">' . $tmp[1] . '</th>';
+           }
+
+
+          foreach($this -> revenue as $key=>$val){
+            echo '<tr>';
+            foreach($ARI as $ref){
+              if($ref=='_lgn_time')
+                echo '<td>'.date('Y-m-d @ H:i',$val[$ref]).'</td>';
+              else 
+                echo '<td>'.$val[$ref].'</td>';
+            }
+            echo '</tr>';
+          }
+        ?>
+      </table>
     </div>
 
     <div class="bodyworks">
-      <h2>Part Two</h2>
-      <?php
-        echo $this -> data2;
-      ?>
+      <h2>Expenses</h2>
+      <table class="reports-table">
+        <?php
+          foreach($AEH as $k){
+            $tmp = explode(',' ,$k);
+            print '<th class="' . $tmp[0] . '">' . $tmp[1] . '</th>';
+           }
+
+
+          foreach($this -> expense as $key=>$val){
+            echo '<tr>';
+            foreach($AEI as $ref){
+              if($ref=='_lgn_time')
+                echo '<td>'.date('Y-m-d @ H:i',$val[$ref]).'</td>';
+              else 
+                echo '<td>'.$val[$ref].'</td>';
+            }
+            echo '</tr>';
+          }
+        ?>
+      </table>
     </div>
 
     <div class="bodyworks">
-      <h2>Part Three</h2>
-      <?php
-        echo $this -> data3;
-      ?>
+      <h2>Enrollments</h2>
+      <table class="reports-table">
+        <?php
+
+          foreach($ERH as $k){
+            $tmp = explode(',' ,$k);
+            print '<th class="' . $tmp[0] . '">' . $tmp[1] . '</th>';
+           }
+          foreach($this -> enrollment as $key=>$val){
+            echo '<tr>';
+            foreach($ERI as $ref){
+              if($ref=='_cust_addedon')
+                echo '<td>'.date('Y-m-d @ H:i',$val[$ref]).'</td>';
+              else 
+                echo '<td>'.$val[$ref].'</td>';
+            }
+            echo '</tr>';
+          }
+        ?>
+      </table>
     </div>
 
 <?php
